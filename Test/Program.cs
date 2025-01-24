212d496ecada8109d70fdfaff32e00c1715d7641
Amrit1234567 <sharma.amrit9861@gmail.com>
2025-01-24 08:26:16 +0545

    Initial commit

---
+++ Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Test.Models;
+
+namespace Test
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer myComputer = new Computer()
+            {
+                Motherboard="Z460",
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = DateTime.Now,
+                Price = 1000.47m,
+                VideoCard = "RTX 4050"
+            };
+            Console.WriteLine(myComputer.Motherboard);
+            
+        }
+    }
+}
