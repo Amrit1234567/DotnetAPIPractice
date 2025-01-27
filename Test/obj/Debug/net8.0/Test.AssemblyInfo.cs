4cbd7587a9db0be3e383ef74fb24edcab0fc00eb
Amrit1234567 <sharma.amrit9861@gmail.com>
2025-01-27 20:15:58 +0545

    a

--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc1b333b3034951c5c72fb769b2403d38689b974")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5919a2af3f16129920752f1c37e436f7c425badf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
